0c7ccde5747ca5b77754eaec049359760673bf36
Igor Borisov <latentpickuper@gmail.com>
2019-07-04 10:26:29 +0300

    init less8

--- lesson7/config/paths.config.php
+++ lesson7/config/paths.config.php
@@ -3,3 +3,4 @@ define('ROOT_DIR', __DIR__ . '/../');
 define('PUBLIC_DIR', ROOT_DIR . 'public_html/');
 define('ENGINE_DIR', ROOT_DIR . 'engine/');
 define('TEMPLATES_DIR', ROOT_DIR . 'templates/');
+define('API_DIR', ROOT_DIR . 'api/');
